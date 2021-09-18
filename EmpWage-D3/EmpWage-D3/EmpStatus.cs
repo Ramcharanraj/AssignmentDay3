5aa46d2d95cb5de1cecad421b7d16920f621931e
Ramcharanraj <Ramcharanraj94@gmail.com>
2021-09-19 02:57:26 +0530

    Add: Present or Absent

---
+++ EmpWage-D3/EmpWage-D3/EmpStatus.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace EmpWage_D3
+{
+    public class EmpStatus
+    {
+        public EmpStatus()
+        {
+        }
+    }
+}
--- EmpWage-D3/EmpWage-D3/Program.cs
+++ EmpWage-D3/EmpWage-D3/Program.cs
@@ -7,6 +7,18 @@ namespace EmpWage_D3
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome Employee's");
+            int IS_FULL_TIME = 1 ;
+            Random random = new Random(); 
+
+            int empCheck = random.Next(0, 2);
+            if(empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
