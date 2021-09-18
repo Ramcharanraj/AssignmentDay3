a87095d72f4ac84aec90a325dcc6810d15a851bb
Ramcharanraj <Ramcharanraj94@gmail.com>
2021-09-19 02:33:49 +0530

    Add:Welcome message

---
+++ EmpWage-D3/EmpWage-D3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmpWage_D3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Employee's");
+        }
+    }
+}
