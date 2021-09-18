285d61d7dbc1d56e8d92ad8cec8b17b9b9c52d20
Ramcharanraj <Ramcharanraj94@gmail.com>
2021-09-19 03:12:09 +0530

    Add: Emp-Wage

--- EmpWage-D3/EmpWage-D3/Program.cs
+++ EmpWage-D3/EmpWage-D3/Program.cs
@@ -1,4 +1,4 @@
-:using System;
+using System;
 
 namespace EmpWage_D3
 {
@@ -8,17 +8,24 @@ namespace EmpWage_D3
         {
             Console.WriteLine("Welcome Employee's");
             int IS_FULL_TIME = 1 ;
+            int EMP_RATE_PER_HOUR = 20;
+
+            int empHrs = 0;
+            int empWage = 0;
+
             Random random = new Random(); 
 
             int empCheck = random.Next(0, 2);
-            if(empCheck == IS_FULL_TIME)
+            if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is present");
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage: " +empWage);
         }
     }
 }
