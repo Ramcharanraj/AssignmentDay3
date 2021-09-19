94dd84ce8c3c27fa493df0b145129c8ba1919ee8
Ramcharanraj <Ramcharanraj94@gmail.com>
2021-09-19 16:33:14 +0530

    Add:Part-time-wage

--- EmpWage-D3/EmpWage-D3/Program.cs
+++ EmpWage-D3/EmpWage-D3/Program.cs
@@ -4,26 +4,39 @@ namespace EmpWage_D3
 {
     class Program
     {
+        private static object elseif;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome Employee's");
-            int IS_FULL_TIME = 1 ;
+            int IS_FULL_TIME = 2 ;
+            int IS_PART_TIME = 1 ;
             int EMP_RATE_PER_HOUR = 20;
+           
+
 
             int empHrs = 0;
             int empWage = 0;
 
             Random random = new Random(); 
 
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
             {
+                Console.WriteLine("The Part-time-wage is ");
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("The Full-Time-Wage is ");
                 empHrs = 8;
             }
             else
             {
                 empHrs = 0;
+                Console.WriteLine("Employee is absent"); ;
             }
+
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Emp Wage: " +empWage);
         }
